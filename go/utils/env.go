cf1bd6c94182942e5d7a2ecec5d7b42085ec4300
BANKA2017 <17263253+BANKA2017@users.noreply.github.com>
2026-04-24 00:36:03 +0800

    [go] add: func `GetIntEnv` and `GetBoolEnv`

--- go/utils/env.go
+++ go/utils/env.go
@@ -1,10 +1,37 @@
 package utils
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
-func GetEnv(key, fallback string) string {
+func GetEnv(key, defaultValue string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
 	}
-	return fallback
+	return defaultValue
+}
+
+var envFalseValues = map[string]struct{}{
+	"0": {}, "false": {}, "off": {}, "disabled": {}, "no": {}, "n": {},
+}
+
+func GetBoolEnv(key string) bool {
+	var envVal = GetEnv(key, "")
+	_, isFalse := envFalseValues[envVal]
+	return !(envVal == "" || isFalse)
+}
+
+func GetIntEnv(key string, defaultValue int) int {
+	var envVal = GetEnv(key, "")
+	if envVal == "" {
+		return defaultValue
+	}
+
+	val, err := strconv.Atoi(envVal)
+	if err != nil {
+		return defaultValue
+	}
+
+	return val
 }
